433d87dc3b4ab8425d55f7c867e1b075b6125afe
faizanasif728 <fasif4832@gmail.com>
2024-07-11 13:47:23 +0500

    07/11/2024 - practiced variables in Js

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-const main =()=>{
-    console.log("main function");
+const main = () => {
+  console.log("main function");
 };
 
-const func1 =()=>{
-    console.log("func1");
+const func1 = () => {
+  console.log("func1");
 };
 
-const func2 =()=>{
-    console.log("func2");
+const func2 = () => {
+  console.log("func2");
 };
 
-const func4 =()=>{
-    console.log("func4");
+const func4 = () => {
+  console.log("func4");
 };
-
