4142528f13e067a0db56a83a98a9bd414585d4d2
faizanasif728 <fasif4832@gmail.com>
2024-07-28 17:57:59 +0500

    07/28/2024 - clock_commit

--- clock.js
+++ clock.js
@@ -1,19 +1,26 @@
+//  Assigning arrow function to constant variable named as "mypromise"
 const mypromise = () => {
+  // inside arrow function there is promise that respond time of 1 sec interval
   return new Promise((res) => {
     setTimeout(() => {
       res(new Date());
     }, 1000);
   });
 };
+// Asynchronous Function named as "main"
 const main = async () => {
+  // For loop for infinite iterations
   for (let i = 0; ; i++) {
     try {
+      // calling mypromise function
       await mypromise().then((res) => {
         console.log(res);
       });
+      // If there is any error
     } catch (error) {
       console.log(error);
     }
   }
 };
+// calling main function
 main();
